41dc61ac55bbe4b9907dede65db72d7ca4e8f93c
John <johnpops59@gmail.com>
2025-04-21 16:57:55 +0100

    returning expressions with  a lambda expression

--- lambdaSam.java
+++ lambdaSam.java
@@ -6,10 +6,19 @@ interface Big
     void show(int i);
 }
 
+interface A
+{
+    int add(int i, int j);
+}
+
 public class lambdaSam {
     public static void main(String a[])
     {
         Big obj = i -> System.out.println("in show : " + i); // lambda expression
         obj.show(10);
+
+        A obj1 = (i, j) -> i + j;
+        int result = obj1.add(10, 7);
+        System.out.println("result: " + result);
     }
 }
