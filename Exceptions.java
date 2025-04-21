67cfcbace1c8b211df9ba28b23ecbc6f386d48ce
John <johnpops59@gmail.com>
2025-04-21 18:35:17 +0100

    handling java exceptions - try_catch

--- Exceptions.java
+++ Exceptions.java
@@ -3,18 +3,18 @@
 public class Exceptions {
     public static void main(String a[])
     {
-        int d = 11;
+        int d = 0;
         int b = 0;
 
         try
         {
             b = 121/d;
+            System.out.println("code run smoothly, result of 121" + "/" + d + " = " + b);
         }
         catch(Exception e)
         {
             System.out.println("Error while running...");
+            System.out.println("bye");
         }
-
-        System.out.println("code run smoothly, result of 121" + "/" + d + " = " + b);
     }
 }
