8f892d70adfdf5f8954223fe125a32a6fc8b0b1c
John <johnpops59@gmail.com>
2025-05-15 12:57:49 +0100

    unit testing with maven

---
+++ Junit01/src/main/java/com/acmetech/learning/Calc.java
@@ -0,0 +1,8 @@
+package com.acmetech.learning;
+
+public class Calc {
+	public int divide(int num1, int num2)
+	{
+		return num1/num2;
+	}
+}
---
+++ Junit01/src/test/java/com/acmetech/learning/CalcTest.java
@@ -0,0 +1,17 @@
+package com.acmetech.learning;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void test() {
+		Calc c = new Calc();
+		int actual = c.divide(10, 5);
+		int expected = 2;
+		assertEquals(expected, actual);
+	}
+
+}
