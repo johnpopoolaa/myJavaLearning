fc58a0720c4390bc828864872de447a01908f0eb
John Popoola <johnpops59@gmail.com>
2026-02-20 17:41:46 +0100

    servlet filters

---
+++ DemoApp/src/com/acmetech/NameFilter.java
@@ -0,0 +1,47 @@
+package com.acmetech;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class NameFilter
+ */
+@WebFilter("/addAlien")
+public class NameFilter implements Filter {
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		PrintWriter out = response.getWriter();
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		String aname = request.getParameter("aname");
+		int length = aname.length();
+		
+		if(length>1)
+			chain.doFilter(request, response);
+		else
+			out.print("name should be longer than 1 character");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
