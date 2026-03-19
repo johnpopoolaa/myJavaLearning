9bf528886e8151c14190e948cc7d3c1aac5faa71
John Popoola <johnpops59@gmail.com>
2026-03-19 15:40:21 +0100

    using JDBC and mysql db to login

--- servlet/LoginModule/src/com/login/Login.java
+++ servlet/LoginModule/src/com/login/Login.java
@@ -8,15 +8,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.login.dao.LoginDao;
+
 
 @WebServlet("/Login")
 public class Login extends HttpServlet {
+	private static final long serialVersionUID =1L;
+	
+	public Login() {
+		super();
+	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uname = request.getParameter("uname");
 		String pass = request.getParameter("pass");
 		
-		if(uname.equals("john") && pass.equals("codelixer"))
+		LoginDao dao = new LoginDao();
+		
+		if(dao.check(uname, pass))
 		{
 			HttpSession session = request.getSession();
 			session.setAttribute("username", uname);
