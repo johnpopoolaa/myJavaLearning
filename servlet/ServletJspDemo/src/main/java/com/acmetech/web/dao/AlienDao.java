c79c545be6eae29e613b8623be6c6e750adb625e
John Popoola <johnpops59@gmail.com>
2026-04-27 23:12:47 +0100

    get data from a database to use with servlet

--- servlet/ServletJspDemo/src/main/java/com/acmetech/web/dao/AlienDao.java
+++ servlet/ServletJspDemo/src/main/java/com/acmetech/web/dao/AlienDao.java
@@ -1,15 +1,28 @@
 package com.acmetech.web.dao;
 
+import java.sql.*;
 import com.acmetech.web.model.Alien;
 
 public class AlienDao {
 	public Alien getAlien(int aid)
 	{
 		Alien a = new Alien();
-		a.setAid(101);
-		a.setAname("John");
-		a.setTech("Java");
 		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/john", "root", "#$@123John");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from alien where aid=" + aid);
+			if(rs.next()) // move to the next row
+			{
+				a.setAid(rs.getInt("aid"));
+				a.setAname(rs.getString("aname"));
+				a.setTech(rs.getString("tech"));
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 		return a;
 	}
 }
